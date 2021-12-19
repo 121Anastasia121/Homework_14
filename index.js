75b4fb34c91a18ac68def509b13e4a4d5887598e
121Anastasia121 <anastasia_frantova@mail.ru>
2021-12-19 16:05:21 +0300

    Homework_14

---
+++ index.js
@@ -0,0 +1,16 @@
+let input = document.querySelector('input');
+let myNumder = 10;
+
+let sendNumber = function() {
+  if (input.value === myNumber) {
+    alert ('you right');
+  };
+
+  if (input.value < myNumber) {
+    alert ('add more');
+  };
+
+  if (input.value > myNumber) {
+    alert ('add less');
+  };
+};
